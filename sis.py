e43203c249e1280baf9f73d49b24c91d0342f7bc
polinamasenko <137270511+polinamasenko@users.noreply.github.com>
2023-06-22 16:04:00 +0200

    Create sis.py
    add class

---
+++ sis.py
@@ -0,0 +1,14 @@
+
+class MyClass:
+    a = 10
+    def func(self):
+        print('Hello')
+
+# Output: 
+print(MyClass)
+
+# deleting MyClass
+del MyClass
+
+# Error: MyClass is not defined
+print(MyClass)	
